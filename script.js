29a09462ec758b8fe5a436a81d6dcf23043900fe
MATHEUS DA SILVA AMADEU <00001099049908sp@aluno.educacao.sp.gov.br>
2026-08-19 08:10:51 -0300

    feat: adicionar novas funcoes de multiplicacao e divisao

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
 function somar() {
-    let numero1 = Number(document.getElementById("numero1").value);
-    let numero2 = Number(document.getElementById("numero2").value);
+  let numero1 = Number(document.getElementById("numero1").value);
+  let numero2 = Number(document.getElementById("numero2").value);
 
-    let resultado = numero1 + numero2;
+  let resultado = numero1 + numero2;
 
-    document.getElementById("resultado").innerHTML = "Resultado: " + resultado;
+  document.getElementById("resultado").innerHTML = "Resultado: " + resultado;
 }
 
 function subtrair() {
-    let numero1 = Number(document.getElementById("numero1").value);
-    let numero2 = Number(document.getElementById("numero2").value);
+  let numero1 = Number(document.getElementById("numero1").value);
+  let numero2 = Number(document.getElementById("numero2").value);
 
-    let resultado = numero1 - numero2;
+  let resultado = numero1 - numero2;
 
-    document.getElementById("resultado").innerHTML = "Resultado: " + resultado;
+  document.getElementById("resultado").innerHTML = "Resultado: " + resultado;
+}
+
+function multiplicar() {
+  let numero1 = Number(document.getElementById("numero1").value);
+  let numero2 = Number(document.getElementById("numero2").value);
+
+  document.getElementById("resultado").innerText =
+    "Resultado: " + numero1 * numero2;
+}
+
+function dividir() {
+  let numero1 = Number(document.getElementById("numero1").value);
+  let numero2 = Number(document.getElementById("numero2").value);
+
+  if (numero2 === 0) {
+    document.getElementById("resultado").innerText =
+      "Resultado: Não é possível dividir por zero.";
+    return;
+  }
+
+  document.getElementById("resultado").innerText =
+    "Resultado: " + numero1 / numero2;
 }
